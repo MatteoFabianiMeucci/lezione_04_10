366bc5ffac107fe0e4acd92214d2244aaa9d659d
lai2-24 <lai2-24@itismeucci.com>
2024-10-04 09:00:18 +0200

    creazione classi: Cestino, Persona. Problema di conto

---
+++ src/main/java/com/example/Cestino.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class Cestino {
+    private int contatore;
+    
+    public Cestino(int contatore){
+        this.contatore = contatore;
+    }
+
+    public int getContatore() {
+        return contatore;
+    }
+
+    public void aggiungiMoneta(){
+        contatore = contatore + 1;
+    }
+    
+}
